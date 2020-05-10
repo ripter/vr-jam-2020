c26e116abb8baea14995f345c778e6b7fd087e72
ripter <ripter001@gmail.com>
2020-05-10 12:58:58 -0700

    Cleaning up

--- src/shaders/gradient/index.js
+++ src/shaders/gradient/index.js
@@ -4,9 +4,9 @@ import fragmentGradientSimple from './fragment-gradient-simple.glsl';
 
 AFRAME.registerShader('gradient-simple', {
   schema: {
-    topColor: {type: 'color', default: '#red', is: 'uniform'},
-    bottomColor: {type: 'color', default: '#blue', is: 'uniform'}
+    topColor: { type: 'color', default: '#red', is: 'uniform' },
+    bottomColor: { type: 'color', default: '#blue', is: 'uniform' },
   },
-  vertexShader: vertexShader,
+  vertexShader,
   fragmentShader: fragmentGradientSimple,
 });
